05b98f008be467ec0260da7e181d5dc5b1bec602
Harsh <hbaste@newrelic.com>
2023-12-14 07:34:51 +0000

    Better pagination support

--- dump_alerts.py
+++ dump_alerts.py
@@ -10,16 +10,17 @@ account_id = 3493139
 new_relic_api_key = 'NRAK-FJ4LZGDR25FECFSB5UBN6H2LXCN'
 
 query_policies = gql("""
-query getPolicyConditions($accountId: Int!) {
+query getPolicyConditions($accountId: Int!, $cursor: String) {
   actor {
     account(id: $accountId) {
       alerts {
-        policiesSearch {
+        policiesSearch(cursor: $cursor) {
           policies {
             id
             name
             incidentPreference
           }
+          nextCursor
         }
       }
     }
@@ -28,7 +29,7 @@ query getPolicyConditions($accountId: Int!) {
 """)
 
 query_policy_conditions = gql("""
-query getPolicyConditions($accountId: Int!, $policyId: ID!) {
+query getPolicyConditions($accountId: Int!, $policyId: ID!, $cursor: String) {
   actor {
     account(id: $accountId) {
       alerts {
@@ -38,8 +39,10 @@ query getPolicyConditions($accountId: Int!, $policyId: ID!) {
           name
           accountId
         }
-        nrqlConditionsSearch {
+        nrqlConditionsSearch (cursor: $cursor) {
+          nextCursor
           nrqlConditions {
+            policyId
             id
             name
             nrql {
@@ -83,19 +86,26 @@ transport = RequestsHTTPTransport(
     use_json=True,
 )
 client = Client(transport=transport, fetch_schema_from_transport=True)
-
-result = client.execute(query_policies, variable_values={"accountId": account_id})
-policies = result['actor']['account']['alerts']['policiesSearch']['policies']
+cursor = None
+all_policies = []
 csv_data = []
 
-for policy in policies:
-    policy_id = policy['id']
-    conditions_result = client.execute(query_policy_conditions, variable_values={"policyId": policy_id, "accountId": account_id})
-    conditions = conditions_result['actor']['account']['alerts']['nrqlConditionsSearch']['nrqlConditions']
+while True:
+    result = client.execute(query_policies, variable_values={"accountId": account_id, "cursor": cursor})
+    policies = result['actor']['account']['alerts']['policiesSearch']['policies']
+    all_policies.extend(policies)
+    if result['actor']['account']['alerts']['policiesSearch']['nextCursor'] is None:
+        break
 
-    for condition in conditions:
-        # flattened_data = json_normalize(condition, sep='.')
-        csv_data.append(condition)
+for policy in all_policies:
+    policy_id = policy['id']
+    cursor = None
+    while True:
+        conditions_result = client.execute(query_policy_conditions, variable_values={"cursor": cursor, "policyId": policy_id, "accountId": account_id})
+        conditions = conditions_result['actor']['account']['alerts']['nrqlConditionsSearch']['nrqlConditions']
+        csv_data.extend(conditions)
+        if conditions_result['actor']['account']['alerts']['nrqlConditionsSearch']['nextCursor'] is None:
+            break
 
 with open('alert_policies.csv', 'w', newline='') as file:
     writer = csv.DictWriter(file, fieldnames=csv_data[0].keys())
