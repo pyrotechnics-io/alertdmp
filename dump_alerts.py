acac77b3d767182d33be41174c54f6104186e7c3
Harsh <hbaste@newrelic.com>
2023-12-18 06:05:33 +0000

    Multi-account

--- dump_alerts.py
+++ dump_alerts.py
@@ -13,11 +13,12 @@ from gql.transport.exceptions import TransportQueryError
 import logging
 
 # GraphQL templates
+query_accounts = None
 query_policies = None
 query_policy_conditions = None
 
 # Http
-max_retries = 10
+max_retries = 3
 retry_delay = 5
 
 def setup_module_logger(name, log_level):
@@ -32,17 +33,37 @@ def setup_module_logger(name, log_level):
 
 
 def load_templates():
+    global query_accounts
     global query_policies
     global query_policy_conditions
 
+    with open("accounts.gql", 'r') as acc:
+        query_accounts = gql(acc.read())
     with open("policies.gql", 'r') as pol:
         query_policies = gql(pol.read())
     with open("policy_conditions.gql", 'r') as cond:
         query_policy_conditions = gql(cond.read())
 
-def process(account_id, new_relic_api_key):
+def query(client, query, parameters):
     global max_retries
     global retry_delay
+    result = None
+    retries = max_retries
+    while True:
+        try:
+            result = client.execute(query, variable_values=parameters)
+            return result
+        except Exception as e:
+            if retries == 0:
+                logger.error("Maximum retries exceeded. Last error: {}", e)
+                return None
+            else:
+                retries -= 1
+                logger.debug("Retry {} on error: {}".format(retries, e))
+            time.sleep(retry_delay)
+            continue
+
+def process(account_id, new_relic_api_key):
     transport = RequestsHTTPTransport(
         url='https://api.newrelic.com/graphql',
         headers={'API-Key': new_relic_api_key},
@@ -52,60 +73,51 @@ def process(account_id, new_relic_api_key):
     cursor = None
     all_policies = []
     csv_data = []
+    all_accounts = []
 
-    logger.info("Looking for all configured policies")
-    retries = max_retries
-    while True:
-        try:
-            result = client.execute(query_policies, variable_values={"accountId": account_id, "cursor": cursor})
-        except Exception as e:
-            if retries == 0:
-                logger.error("Maximum retries exceeded. Last error: {}", e)
-                os._exit(-1)
-            else:
-                retries -= 1
-                logger.debug("Retry {} on HTTP error: {}".format(retries, e))
-            time.sleep(retry_delay)
-            continue
-        policies = result['actor']['account']['alerts']['policiesSearch']['policies']
-        all_policies.extend(policies)
-        cursor = result['actor']['account']['alerts']['policiesSearch']['nextCursor']
-        if cursor is None:
-            break
+    if account_id:
+        all_accounts.append(account_id)
+    else:
+        # Look for all accounts accessible by this key
+        result = query(client, query_accounts, None)
+        for acc in result['actor']['accounts']:
+            all_accounts.append(acc['id'])
 
-    for policy in all_policies:
-        policy_id = policy['id']
-        policy_name = policy['name']
-        cursor = None
-        logger.info("Looking for conditions inside policy: {}".format(policy_id))
+    for account_id in all_accounts:
+        logger.info(f"Looking for configured policies on account {account_id}")
         retries = max_retries
         while True:
-            try:
-                logger.debug("Querying account: {} policy: {}|{}".format(account_id, policy_id, policy_name))
-                conditions_result = client.execute(query_policy_conditions, variable_values={"cursor": cursor, "policyId": policy_id, "accountId": account_id})
-            except Exception as e:
-                if retries == 0:
-                    logger.error("Maximum retries exceeded. Last error: {}", e)
-                    os._exit(-1)
-                else:
-                    retries -= 1
-                    logger.debug("Retry {} on error: {}".format(retries, e))
-                time.sleep(retry_delay)
-                continue
-            conditions = conditions_result['actor']['account']['alerts']['nrqlConditionsSearch']['nrqlConditions']
-            logger.debug(json.dumps(conditions, indent=4))
-            # Need to add the policy name to all of them. 
-            for c in conditions:
-                c["policyName"] = policy_name
-            csv_data.extend(conditions)
-            cursor = conditions_result['actor']['account']['alerts']['nrqlConditionsSearch']['nextCursor']
+            result = query(client, query_policies, {"accountId": account_id, "cursor": cursor})
+            policies = result['actor']['account']['alerts']['policiesSearch']['policies']
+            all_policies.extend(policies)
+            cursor = result['actor']['account']['alerts']['policiesSearch']['nextCursor']
             if cursor is None:
                 break
+
+        for policy in all_policies:
+            policy_id = policy['id']
+            policy_name = policy['name']
+            cursor = None
+            logger.info("Looking for conditions inside policy: {}".format(policy_id))
+            while True:
+                logger.debug("Querying account: {} policy: {}|{}".format(account_id, policy_id, policy_name))
+                conditions_result = query(client, query_policy_conditions, {"cursor": cursor, "policyId": policy_id, "accountId": account_id})
+                if conditions_result is None:
+                    break # It was an un-recoverable error. Just move to the next policy
+                conditions = conditions_result['actor']['account']['alerts']['nrqlConditionsSearch']['nrqlConditions']
+                logger.debug(json.dumps(conditions, indent=4))
+                # Need to add the policy name to all of them. 
+                for c in conditions:
+                    c["policyName"] = policy_name
+                csv_data.extend(conditions)
+                cursor = conditions_result['actor']['account']['alerts']['nrqlConditionsSearch']['nextCursor']
+                if cursor is None:
+                    break
     return csv_data
 
 def get_args():
     parser = argparse.ArgumentParser(description="Alert configuration dumper")
-    parser.add_argument("--account_id", required=True, help="Your New Relic account ID")
+    parser.add_argument("--account_id", required=False, help="Your New Relic Account ID (leave blank for all accounts)")
     parser.add_argument("--api_key", required=True, help="Your New Relic API key")
     parser.add_argument("--output_file", required=False, default="alert_policies.csv", help="Output file")
     parser.add_argument("--json", required=False, action='store_true', default=False, help="Dump json directly to file")
@@ -122,7 +134,11 @@ def main():
       logger = setup_module_logger(__name__, logging.DEBUG)
     else:
       logger = setup_module_logger(__name__, logging.INFO)
-    account_id = int(args.account_id)
+    
+    account_id = None
+    if args.account_id:
+        account_id = int(args.account_id)
+
     api_key = args.api_key
     logger.info("Running on account {}".format(args.account_id))
     
